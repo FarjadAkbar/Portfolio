df92255cf514198a8c6461bc9416a985dce7a20a
Farjad <farjadakbar4@gmail.com>
2024-10-21 01:07:08 +0500

    feat: add cv

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
         <span className="font-bold">Hello, I'm Farjad.</span> I'm a{" "}
         <span className="font-bold">full-stack engineer</span> with{" "}
         <span className="font-bold">4 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
+        building <span className="italic">sites & apps</span>. My focus on developing{" "}
         <span className="underline">MERN & Laravel</span>.
       </motion.h1>
 
@@ -91,7 +91,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/Farjad-Akbar-CV.pdf"
           download
         >
           Download CV{" "}
